5d90a3fbece801d5b9770993af652c6a27ebc7d9
David P Thomas <dthomas@rallydev.com>
2014-09-12 10:37:57 -0700

    removed gauge chart; nice try though\!

--- SanityDashboard/App.js
+++ SanityDashboard/App.js
@@ -67,7 +67,6 @@ Ext.define('CustomApp', {
       this._buildPieChart();
       this._buildBubbleChart();
       this._buildColumnChart();
-      this.gaugeChart();
     },
 
     _buildPieChart: function() {
@@ -222,92 +221,6 @@ Ext.define('CustomApp', {
       });
 
     },
-
-        gaugeChart: function() {
-         
-        var gaugeConfig = {
-        
-            chart: {
-            type: 'solidgauge'
-            },
-
-            title: null,
-
-            pane: {
-                center: ['50%', '85%'],
-                size: '140%',
-                startAngle: -90,
-                endAngle: 90,
-                background: {
-                    backgroundColor: /*(Highcharts.theme && Highcharts.theme.background2) ||*/ '#EEE',
-                    innerRadius: '60%',
-                    outerRadius: '100%',
-                    shape: 'arc'
-                }
-            },
-
-            tooltip: {
-                enabled: false
-            },
-
-            // the value axis
-            yAxis: {
-                min: 0,
-                max: 100,
-                title: {
-                       text: 'foo'
-                    },
-                stops: [
-                    [0.1, '#55BF3B'], // green
-                    [0.5, '#DDDF0D'], // yellow
-                    [0.9, '#DF5353'] // red
-                ],
-                lineWidth: 0,
-                minorTickInterval: null,
-                tickPixelInterval: 400,
-                tickWidth: 0,
-                title: {
-                    y: -70
-                },
-                labels: {
-                    y: 16
-                }
-            },
-
-            plotOptions: {
-                solidgauge: {
-                    dataLabels: {
-                        y: 5,
-                        borderWidth: 0,
-                        useHTML: false
-                    }
-                }
-            }
-        };
-        
-                   
-   
-      this.down('#ribbon').add({
-        xtype: 'rallychart',
-        chartConfig: gaugeConfig,
-        chartData: {
-            series: [{
-                name: 'MisterFooBar',
-                data: [1],
-                dataLabels: {
-                format: '<div style="text-align:center"><span style="font-size:25px;color:' + 'black' 
-                        + '">{y:.1f}</span><br/>' +
-                       '<span style="font-size:12px;color:silver">* 1000 / min</span></div>'
-                },
-                tooltip: {
-                    valueSuffix: ' spins/min'
-                }   
-            }]
-            }   
-        });
-    
-    },
-
 
     // Create all grids in the left/right columns
     _buildGrids: function() {
