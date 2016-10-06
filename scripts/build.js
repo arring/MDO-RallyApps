f715b67e05e0bf3811e17bfd68ff3813a94f6cd3
Sarita Regmi <sr@nowhere.com>
2016-10-06 15:00:37 -0700

    replacing rab by rally-app-build build so that it works for trying to build the app

--- scripts/build.js
+++ scripts/build.js
@@ -42,7 +42,7 @@ function buildEachProject(configFiles, buildCommand, srcFile, deployDir){
 	});
 }
 function buildEachRabProject(){
-	buildEachProject(getFiles('config.json'), 'rab', 'deploy/App-uncompressed.html', rabDeployDir);
+	buildEachProject(getFiles('config.json'), 'rally-app-builder build', 'deploy/App-uncompressed.html', rabDeployDir);
 }
 function buildEachSmProject(){
 	buildEachProject(getFiles('sm-config.json'), 'sm-rab', 'sm-deploy/sm-app.html', smDeployDir);
